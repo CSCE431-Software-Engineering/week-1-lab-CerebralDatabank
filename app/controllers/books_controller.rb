df05c72df0304c3cf89ae5ffab7c34188b723ab8
Gopal Othayoth <Gopal.Othayoth@outlook.com>
2024-01-27 09:43:13 -0600

    Add flash notice for book actions
    Notice shown for book create, update, and delete

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -14,7 +14,7 @@ class BooksController < ApplicationController
   def create
     @book = Book.new(book_params)
     if @book.save
-      redirect_to books_path
+      redirect_to(books_path, notice: "Book created")
     else
       render('new')
     end
@@ -27,7 +27,7 @@ class BooksController < ApplicationController
   def update
     @book = Book.find(params[:id])
     if @book.update(book_params)
-      redirect_to book_path(@book)
+      redirect_to(book_path(@book), notice: "Book saved")
     else
       render('new')
     end
@@ -40,7 +40,7 @@ class BooksController < ApplicationController
   def destroy
     @book = Book.find(params[:id])
     @book.destroy
-    redirect_to books_path
+    redirect_to(books_path, notice: "Book deleted")
   end
 
   def book_params
