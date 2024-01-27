621d4db83f973771b80b664019ee628d3988e099
Gopal Othayoth <Gopal.Othayoth@outlook.com>
2024-01-27 01:29:11 -0600

    Update books controller to support all actions

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -1,22 +1,51 @@
 class BooksController < ApplicationController
   def index
+    @books = Book.order(:id)
+  end
+
+  def show
+    @book = Book.find(params[:id])
   end
 
   def new
+    @book = Book.new
   end
 
   def create
+    @book = Book.new(book_params)
+    if @book.save
+      redirect_to books_path
+    else
+      render('new')
+    end
   end
 
   def edit
+    @book = Book.find(params[:id])
   end
 
   def update
+    @book = Book.find(params[:id])
+    if @book.update(book_params)
+      redirect_to book_path(@book)
+    else
+      render('new')
+    end
   end
 
   def delete
+    @book = Book.find(params[:id])
   end
 
   def destroy
+    @book = Book.find(params[:id])
+    @book.destroy
+    redirect_to books_path
+  end
+
+  def book_params
+    params.require(:book).permit(
+      :title
+    )
   end
 end
